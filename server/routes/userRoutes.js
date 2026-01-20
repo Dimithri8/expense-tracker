a30917713f22264f47de4bfcbc2f954e516942f6
Dimithri08 <dimithridananjaya924@gmail.com>
2026-01-20 20:44:12 +0530

    Updated createUser file import link

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser } from "../controllers/SignUpController.js";
+import { createUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
